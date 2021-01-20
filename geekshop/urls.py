3a565bf907c8f6831e1da86906edef9b2666d022
Seaman51 <seamanmurmansk@gmail.com>
2021-01-20 19:47:36 +0300

    Edit urls.py

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -21,5 +21,5 @@ from mainapp import views as mainapp_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp_views.index),
-    path('products.html', mainapp_views.products),
+    path('products/', mainapp_views.products),
 ]
